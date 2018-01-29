7bf677498cbdaf8e8417365e61bb9a6745499030
Donghui Zhuo <terryz@Donghuis-MacBook-Pro.local>
2018-01-29 14:58:25 -0800

    Fix issues in TwitterActivityOne and TwitterActivityTwo
    Now we should be available to run TwitterActivityOne and
    TwitterActivityTwo directly. Program arguments (used to
    start the applications) have been documented in the code
    as well.

--- sapphire/examples/example-minnietwitter/src/main/java/sapphire/appexamples/minnietwitter/device/TwitterActivityOne.java
+++ sapphire/examples/example-minnietwitter/src/main/java/sapphire/appexamples/minnietwitter/device/TwitterActivityOne.java
@@ -11,44 +11,59 @@ import sapphire.appexamples.minnietwitter.app.Tweet;
 import sapphire.appexamples.minnietwitter.app.TwitterManager;
 import sapphire.appexamples.minnietwitter.app.User;
 import sapphire.appexamples.minnietwitter.app.UserManager;
+import sapphire.kernel.common.GlobalKernelReferences;
 import sapphire.kernel.server.KernelServer;
 import sapphire.kernel.server.KernelServerImpl;
 import sapphire.oms.OMSServer;
 
 public class TwitterActivityOne {
-	
-	public static void main(String[] args) {
-		Registry registry;
-		try {
-			registry = LocateRegistry.getRegistry(args[0],Integer.parseInt(args[1]));
-			OMSServer server = (OMSServer) registry.lookup("SapphireOMS");
 
-			System.out.println("Connected to the OMS: " + server);
-			
-            KernelServer nodeServer = new KernelServerImpl(new InetSocketAddress(args[0], Integer.parseInt(args[1])), new InetSocketAddress(args[2], Integer.parseInt(args[3])));
-            
-			TwitterManager tm = (TwitterManager) server.getAppEntryPoint();
-            System.out.println("Received Twitter Manager Stub: " + tm);
-            
-            UserManager userManger = tm.getUserManager();
-            userManger.addUser("user12", "pwd");
-            User u = userManger.getUser("user12");
-            Timeline timeline = u.getTimeline();
-            
-            List<Tweet> peerTweets = timeline.getTweets(0, 100);
-            
-            System.out.println("User user10 has " + peerTweets.size() + " tweets");
-            
-            for (Tweet t : peerTweets) {
-            	System.out.println("Tweet: " + t.getText());
-            	System.out.println("No. retweets: " + t.getRetweetes());
-            	System.out.println("No. favorites: " + t.getFavorites());
-            }
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    /**
+     * To execute this application, please pass in three parameters: <OMS-IP> <OMS-Port> <KernelServer-Port>
+     * @param args
+     * <ul>
+     *     <li><code>args[0]</code>:</li> OMS server IP address
+     *     <li><code>args[1]</code>:</li> OMS server Port number
+     *     <li><code>args[2]</code>:</li> Kernel server Port number
+     * </ul>
+     *
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        final String userName = "user12";
+        final int tweetCnt = 10;
+
+        Registry registry = LocateRegistry.getRegistry(args[0], Integer.parseInt(args[1]));
+        OMSServer server = (OMSServer) registry.lookup("SapphireOMS");
+        System.out.println("Connected to the OMS: " + server);
+
+        // This kernel server is a fake kernel server. It is _not_ registered in OMS. Therefore
+        // there will be no Sapphire object on this server. The purpose of creating such a fake
+        // kernel server is to construct a KernelClient (inside the KernelServer object) and to
+        // configure GlobalKernelReferences.nodeServer properly.
+        //
+        // Since this is a fake kernel server, we do not have to use the real IP of this host.
+        // We can use any IP address as long as it does not conflict with OMS IP and the IPs of
+        // other kernel servers. To keep things simple, I hard coded it as "127.0.0.2".
+        GlobalKernelReferences.nodeServer = new KernelServerImpl(new InetSocketAddress("127.0.0.2", Integer.parseInt(args[2])), new InetSocketAddress(args[0], Integer.parseInt(args[1])));
+
+        TwitterManager tm = (TwitterManager) server.getAppEntryPoint();
+        System.out.println("Received Twitter Manager Stub: " + tm);
+
+        UserManager userManger = tm.getUserManager();
+        userManger.addUser(userName, "pwd");
+        User u = userManger.getUser(userName);
+        Timeline timeline = u.getTimeline();
+        for (int i = 0; i < tweetCnt; i++) {
+            timeline.tweet("tweet_" + i);
+        }
+
+        List<Tweet> peerTweets = timeline.getTweets(0, tweetCnt);
+        System.out.println(String.format("User %s has %d tweets", userName, peerTweets.size()));
+
+        for (Tweet t : peerTweets) {
+            System.out.println(String.format("==> Tweet: '%s'; number of retweets: %d; number of favorites: %d", t.getText(), t.getRetweetes(), t.getFavorites()));
+        }
+        System.exit(0);
+    }
 }
-
-//Feed feed = me.getFeed();		
-// Obs. Better to get Feed if used often than call me.getFeed().tweet => two RMIs becomes more expensive
--- sapphire/examples/example-minnietwitter/src/main/java/sapphire/appexamples/minnietwitter/device/TwitterActivityTwo.java
+++ sapphire/examples/example-minnietwitter/src/main/java/sapphire/appexamples/minnietwitter/device/TwitterActivityTwo.java
@@ -16,43 +16,56 @@ import sapphire.kernel.server.KernelServerImpl;
 import sapphire.oms.OMSServer;
 
 public class TwitterActivityTwo {
-	public static void main(String[] args) {
-		Registry registry;
-		try {
-			registry = LocateRegistry.getRegistry(args[0],Integer.parseInt(args[1]));
-			OMSServer server = (OMSServer) registry.lookup("SapphireOMS");
+    /**
+     * To execute this application, please pass in three parameters: <OMS-IP> <OMS-Port> <KernelServer-Port>
+     *
+     * @param args <ul>
+     *             <li><code>args[0]</code>:</li> OMS server IP address
+     *             <li><code>args[1]</code>:</li> OMS server Port number
+     *             <li><code>args[2]</code>:</li> Kernel server Port number
+     *             </ul>
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        Registry registry = LocateRegistry.getRegistry(args[0], Integer.parseInt(args[1]));
+        OMSServer server = (OMSServer) registry.lookup("SapphireOMS");
 
-            KernelServer nodeServer = new KernelServerImpl(new InetSocketAddress(args[0], Integer.parseInt(args[1])), new InetSocketAddress(args[2], Integer.parseInt(args[3])));
-            
-			TwitterManager tm = (TwitterManager) server.getAppEntryPoint();
-            System.out.println("Received Twitter Manager Stub: " + tm);
-            
-            UserManager userManger = tm.getUserManager();
-            TagManager tagManager = tm.getTagManager();
-            
-            User me = userManger.addUser("user1", "user1_password");
-            Timeline myTimeline = me.getTimeline();
-            User peer = userManger.getUser("user1");
-            
-            peer.addFollowing(me);
-            me.addFollower(peer);
-            
-            Timeline peerTimeline = peer.getTimeline();
-            peerTimeline.tweet("peer hello");
-            List<Tweet> peerTweets = peerTimeline.getTweets(0, 1);
-            myTimeline.retweet(peerTweets.get(0));
+        // This kernel server is a fake kernel server. It is _not_ registered in OMS. Therefore
+        // there will be no Sapphire object on this server. The purpose of creating such a fake
+        // kernel server is to construct a KernelClient (inside the KernelServer object) and to
+        // configure GlobalKernelReferences.nodeServer properly.
+        //
+        // Since this is a fake kernel server, we do not have to use the real IP of this host.
+        // We can use any IP address as long as it does not conflict with OMS IP and the IPs of
+        // other kernel servers. To keep things simple, I hard coded it as "127.0.0.2".
+        KernelServer nodeServer = new KernelServerImpl(new InetSocketAddress("127.0.0.2", Integer.parseInt(args[2])), new InetSocketAddress(args[0], Integer.parseInt(args[1])));
 
-            List<Tweet> myTweets = myTimeline.getTweets(0, 1);
-            System.out.println("My tweet:" + myTweets.get(0).getText());
+        TwitterManager tm = (TwitterManager) server.getAppEntryPoint();
+        System.out.println("Received Twitter Manager Stub: " + tm);
 
-            tagManager.addTag("#goodlife", myTweets.get(0));
-            List<Tweet> tweetsForTag = tagManager.getTweetsForTag("#goodlife", 0, 1);
-            System.out.println(tweetsForTag.get(0).getText());
+        UserManager userManger = tm.getUserManager();
+        TagManager tagManager = tm.getTagManager();
 
-            System.out.println("Retweets: " + peerTimeline.getRetweets(peerTweets.get(0).getId(), 0, 1).get(0));
+        User me = userManger.addUser("user1", "user1_password");
+        Timeline myTimeline = me.getTimeline();
+        User peer = userManger.getUser("user1");
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        peer.addFollowing(me);
+        me.addFollower(peer);
+
+        Timeline peerTimeline = peer.getTimeline();
+        peerTimeline.tweet("peer hello");
+        List<Tweet> peerTweets = peerTimeline.getTweets(0, 1);
+        myTimeline.retweet(peerTweets.get(0));
+
+        List<Tweet> myTweets = myTimeline.getTweets(0, 1);
+        System.out.println("My tweet:" + myTweets.get(0).getText());
+
+        tagManager.addTag("#goodlife", myTweets.get(0));
+        List<Tweet> tweetsForTag = tagManager.getTweetsForTag("#goodlife", 0, 1);
+        System.out.println(tweetsForTag.get(0).getText());
+
+        System.out.println("Retweets: " + peerTimeline.getRetweets(peerTweets.get(0).getId(), 0, 1).get(0));
+        System.exit(0);
+    }
 }

e28bb1686e33a3713af0f782c9d5d0f794ac5364
Hong Chen <hong.chen1@huawei.com>
2018-05-07 15:35:33 -0700

    able to run in minimum SO system - 1 oms, 1 combo

---
+++ generators/apps/tx.py
@@ -0,0 +1,5 @@
+package = 'hw.demo'
+app_name = 'fundmover' 
+inFolder = '/sapphire/examples/fundmover/build/classes/java/main/hw/demo/'
+outFolder = '/sapphire/examples/fundmover/src/main/java/hw/demo/stubs'
+

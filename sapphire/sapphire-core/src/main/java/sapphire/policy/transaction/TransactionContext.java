f8116b58f9e0e4da3cc552b18a924b50f1ca7231
Hong Chen <hong.chen1@huawei.com>
2018-05-09 08:57:31 -0700

    supported transaction re-entrance in same thread; avoided infinite loop fanning out primitive messages

--- sapphire/sapphire-core/src/main/java/sapphire/policy/transaction/TransactionContext.java
+++ sapphire/sapphire-core/src/main/java/sapphire/policy/transaction/TransactionContext.java
@@ -1,5 +1,9 @@
 package sapphire.policy.transaction;
 
+import sapphire.policy.SapphirePolicy;
+
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 /**
@@ -8,6 +12,28 @@ import java.util.UUID;
 public class TransactionContext {
     private static final ThreadLocal<UUID> transactionID = new ThreadLocal<UUID>();
     private static final ThreadLocal<TwoPCParticipants> participants = new ThreadLocal<TwoPCParticipants>();
+
+    // nested level of the current transaction
+    private static final ThreadLocal<Integer> transactionEnterCount = new ThreadLocal<Integer>();
+
+    // to keep track of SO objects been processed in the current  transaction primitive op
+    private static final ThreadLocal<Set<SapphirePolicy.SapphireClientPolicy>> processedClients = new ThreadLocal<Set<SapphirePolicy.SapphireClientPolicy>>();
+
+    /**
+     * gets the clients of SO objects that haven been processed so far in the current transaction primitive op
+     * @return the clients of SO objects been processed
+     */
+    public static Set<SapphirePolicy.SapphireClientPolicy> getProcessedClients() {
+        return processedClients.get();
+    }
+
+    /**
+     * resets the processed SO clients
+     */
+    public static void initPrecessed() {
+        HashSet<SapphirePolicy.SapphireClientPolicy> emptyClients = new HashSet<SapphirePolicy.SapphireClientPolicy>();
+        processedClients.set(emptyClients);
+    }
 
     /**
      * gets the transaction ID of the active transaction if present
@@ -29,15 +55,26 @@ public class TransactionContext {
      * @param participantManager the participants manager object that manages transaction participants
      */
     public static void enterTransaction(UUID transactionId, TwoPCParticipants participantManager) {
-        transactionID.set(transactionId);
-        participants.set(participantManager);
+        if (transactionId.equals(getCurrentTransaction())){
+            transactionEnterCount.set(transactionEnterCount.get() + 1);
+        }else{
+            transactionID.set(transactionId);
+            participants.set(participantManager);
+            transactionEnterCount.set(1);
+            initPrecessed();
+        }
     }
 
     /**
      * leaves the transaction after clean up the transaction related data
      */
     public static void leaveTransaction() {
-        transactionID.remove();
-        participants.remove();
+        if (transactionEnterCount.get() != null) {
+            transactionEnterCount.set(transactionEnterCount.get() - 1);
+            if (transactionEnterCount.get() == 0) {
+                transactionID.remove();
+                participants.remove();
+            }
+        }
     }
 }

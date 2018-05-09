3f45d073dc8831bb090bfbc8bca609f93d667e0d
Hong Chen <hong.chen1@huawei.com>
2018-05-09 13:45:14 -0700

    demo - xRes w/ dummy resource

--- sapphire/examples/fundmover/src/main/java/hw/demo/bankaccount.java
+++ sapphire/examples/fundmover/src/main/java/hw/demo/bankaccount.java
@@ -1,11 +1,16 @@
 package hw.demo;
 
 import sapphire.app.SapphireObject;
+import sapphire.policy.serializability.TransactionAlreadyStartedException;
+import sapphire.policy.transaction.TransactionExecutionException;
+import sapphire.policy.transaction.TransactionManager;
 import sapphire.policy.transaction.TwoPCCohortPolicy;
+import sapphire.policy.transaction.TwoPCExtResourceCohortPolicy;
 
 import java.io.Serializable;
+import java.util.UUID;
 
-public class bankaccount implements SapphireObject<TwoPCCohortPolicy> {
+public class bankaccount implements SapphireObject<TwoPCExtResourceCohortPolicy>, TransactionManager {
     private int balance;
 
     public void credit(int amount) {
@@ -18,5 +23,31 @@ public class bankaccount implements SapphireObject<TwoPCCohortPolicy> {
 
     public int getBalance() {
         return this.balance;
+    }
+
+    @Override
+    public void join(UUID transactionId) throws TransactionAlreadyStartedException {
+        System.out.println("xa join");
+    }
+
+    @Override
+    public void leave(UUID transactionId) {
+        System.out.println("xa leave");
+    }
+
+    @Override
+    public Vote vote(UUID transactionId) throws TransactionExecutionException {
+        System.out.println("xa vote");
+        return Vote.YES;
+    }
+
+    @Override
+    public void commit(UUID transactionId) {
+        System.out.println("xa commit");
+    }
+
+    @Override
+    public void abort(UUID transactionId) {
+        System.out.println("xa abort");
     }
 }
--- sapphire/examples/fundmover/src/main/java/hw/demo/wallet.java
+++ sapphire/examples/fundmover/src/main/java/hw/demo/wallet.java
@@ -1,11 +1,16 @@
 package hw.demo;
 
 import sapphire.app.SapphireObject;
+import sapphire.policy.serializability.TransactionAlreadyStartedException;
+import sapphire.policy.transaction.TransactionExecutionException;
+import sapphire.policy.transaction.TransactionManager;
 import sapphire.policy.transaction.TwoPCCohortPolicy;
+import sapphire.policy.transaction.TwoPCExtResourceCohortPolicy;
 
 import java.io.Serializable;
+import java.util.UUID;
 
-public class wallet implements SapphireObject<TwoPCCohortPolicy> {
+public class wallet implements SapphireObject<TwoPCExtResourceCohortPolicy>, TransactionManager {
     private int balance;
 
     public void credit(int amount) {
@@ -21,5 +26,31 @@ public class wallet implements SapphireObject<TwoPCCohortPolicy> {
 
     public int getBalance() {
         return this.balance;
+    }
+
+    @Override
+    public void join(UUID transactionId) throws TransactionAlreadyStartedException {
+        System.out.println("[wallet] xa join");
+    }
+
+    @Override
+    public void leave(UUID transactionId) {
+        System.out.println("[wallet] xa leave");
+    }
+
+    @Override
+    public Vote vote(UUID transactionId) throws TransactionExecutionException {
+        System.out.println("[wallet] xa vote");
+        return Vote.YES;
+    }
+
+    @Override
+    public void commit(UUID transactionId) {
+        System.out.println("[wallet] xa commit");
+    }
+
+    @Override
+    public void abort(UUID transactionId) {
+        System.out.println("[wallet] xa abort");
     }
 }

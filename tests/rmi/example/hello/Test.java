f44d611e70f73416d43b4a9208905f854481d857
Ubuntu <kaveh.shafiee@huawei.com>
2018-06-11 23:59:04 +0000

    Cleaned the repo per User Experience Requirements

--- tests/rmi/example/hello/Test.java
+++
@@ -1,9 +0,0 @@
-package example.hello;
-	
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
-	
-public interface Test extends Remote {
-    public String sayTest() throws RemoteException;
-}

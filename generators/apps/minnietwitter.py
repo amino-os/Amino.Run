1147e423b2ac0044dfc9c1448a3c34fd55780729
Ubuntu <kaveh.shafiee@huawei.com>
2018-06-15 23:24:22 +0000

    Added back generators/ & dependencies/

---
+++ generators/apps/minnietwitter.py
@@ -0,0 +1,4 @@
+package = 'sapphire.appexamples.minnietwitter.app'
+app_name = 'MinnieTwitter' 
+inFolder = '/../example_apps/MinnieTwitter/bin/classes/sapphire/appexamples/minnietwitter/app/'
+outFolder = '/../example_apps/MinnieTwitter/src/sapphire/appexamples/minnietwitter/app/stubs/'

fca0493d1d2289ab7561e7fbdb7be0599251fbef
Ubuntu <kaveh.shafiee@huawei.com>
2018-06-22 20:37:18 +0000

    Cleaned the repo

--- deployment/app.py
+++
@@ -1,7 +0,0 @@
-app_class = "sapphire.appexamples.hankstodo.cloud.TodoStart"
-app_name = "HanksTodo"
-app_client = "sapphire.appexamples.hankstodo.device.TodoActivity"
-
-app_class = "sapphire.appexamples.minnietwitter.cloud.MinnieTwitterStart"
-app_name = "MinnieTwitter"
-app_client = "sapphire.appexamples.minnietwitter.device.generator.TwitterWorldGenerator"
--- deployment/servers.py
+++
@@ -1,7 +0,0 @@
-servers = [("mandel", "11345"),
-           ("mcdonald", "11345"),
-           ("moranis", "11345")]
-
-clients = [("mcdonald", "11346")]
-
-oms = ("mcdonald", "1100")

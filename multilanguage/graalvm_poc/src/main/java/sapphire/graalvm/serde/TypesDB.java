7130134624ddb7b23911d47bed691092517a2278
haibinxie <haibinxie@haibinxies-MacBook-Pro.local>
2018-09-13 13:02:50 -0700

    add graal serde library

---
+++ multilanguage/graalvm_poc/src/main/java/sapphire/graalvm/serde/TypesDB.java
@@ -0,0 +1,63 @@
+package sapphire.graalvm.serde;
+
+import org.graalvm.polyglot.*;
+import java.util.*;
+
+public class TypesDB {
+	
+	public enum Types {
+		BOOLEAN, NATIVE_POINTER, NULL, NUMBER, STRING, SEEN, DYNAMIC_START
+	}
+	public static final Types[] fromInt = Types.values();
+
+	private static List<Type> typesById = new ArrayList<Type>();
+  private static Map<String, Short> typesByName = new HashMap<String, Short>();
+	
+	public static void register(Value prototype) {
+		Type t = new Type(prototype);
+    typesByName.put(prototype.getMetaObject().toString(), (short)(typesById.size() + fromInt.length));
+    typesById.add(t);
+	}
+
+	public static short getTypeID(Value v) {
+    return typesByName.getOrDefault(v.getMetaObject().toString(), (short)-1);
+	}
+
+	public static Value newInstance(short id, Context c) {
+		if(id < fromInt.length) 
+        throw new IllegalArgumentException("cannot instantiate primitive. got code: " + id);
+		return typesById.get(id - fromInt.length).newInstance(c);
+	}
+
+	public static List<String> getMembers(short id) {
+		if(id < fromInt.length)
+			return new ArrayList<String>();
+
+		return typesById.get(id - fromInt.length).members;
+	}
+
+
+	public static class Type {
+		public List<String> members;
+		private Value prototype;
+
+		public Type(Value prototype) {
+			this.prototype = prototype;
+			this.members = new ArrayList<String>();
+
+			for(String m : new TreeSet<String>(prototype.getMemberKeys())) {
+				Value v = prototype.getMember(m);
+				if(!v.canExecute()) { //TODO techincally can be executable and data
+					this.members.add(m);
+				} 
+			}
+		}
+
+		public Value newInstance(Context c) {
+			return prototype.getMember("constructor").execute();
+			//Value v = new Value();
+			//return v;
+		}
+	}
+
+}
---
+++ multilanguage/graalvm_poc/src/main/java/sapphire/graalvm/serde/Writer.java
@@ -0,0 +1,95 @@
+package sapphire.graalvm.serde;
+
+import org.graalvm.polyglot.*;
+import java.io.*;
+import java.util.*;
+
+public class Writer {
+
+	//TODO: should do identity better than toString
+	private Map<String, Integer> seenCache;
+	private int seenInd;
+	private DataOutputStream out;
+
+	public Writer(OutputStream os) {
+		out = new DataOutputStream(os);
+	}
+
+	public void write(Value v) throws Exception {
+		seenInd = 0;
+		seenCache = new HashMap<String, Integer>();
+		writeHelper(v);
+	}
+
+	//TODO narrow exception
+	private void writeHelper(Value v) throws Exception {
+		//if (v.canExecute()) return;
+
+		//check if value cached
+		if(seenCache.keySet().contains(v.toString())) {
+			out.writeInt(seenCache.get(v.toString()));
+			out.writeShort(TypesDB.Types.SEEN.ordinal());
+			return;
+		} else {
+			//System.out.println("new value to cache: " + v.toString());
+			seenCache.put(v.toString(),seenInd);
+			out.writeInt(seenInd);
+			seenInd++;
+		}
+
+		//value type switch
+		short typeindex = 0;
+		if(v.isBoolean()) {
+			System.out.println("found boolean: " + v);
+			out.writeShort(TypesDB.Types.BOOLEAN.ordinal());
+			out.writeBoolean(v.asBoolean());
+		} else if(v.isNativePointer()) {
+			throw new IllegalArgumentException("native pointer not supported");
+		} else if(v.isNull()) {
+			System.out.println("found null");
+			out.writeShort(TypesDB.Types.NULL.ordinal());
+		} else if(v.isNumber()) {
+			System.out.println("found number (assuming int): " + v);
+			out.writeShort(TypesDB.Types.NUMBER.ordinal());
+			out.writeInt(v.asInt());
+		} else if(v.isString()) {
+			System.out.println("found string: " + v);
+			out.writeShort(TypesDB.Types.STRING.ordinal());
+			out.writeUTF(v.asString());
+		} else { //isComplex
+			typeindex = TypesDB.getTypeID(v);
+			System.out.println("found non-primitive, typeindex " + typeindex + " canExecute " + v.canExecute() + " " + v);
+			out.writeShort(typeindex);
+		}
+
+		//check array members
+		if(v.hasArrayElements()) {
+			System.out.println("has array of length " + v.getArraySize());
+			out.writeLong(v.getArraySize());
+			for(long i = 0; i < v.getArraySize(); i++) {
+				System.out.println("writing array element " + i);
+				writeHelper(v.getArrayElement(i));
+			}
+		} else {
+			out.writeLong(0);
+		}
+
+		//check named members
+		if(v.hasMembers()) {
+			List<String> keys = TypesDB.getMembers(typeindex);
+			//System.out.println("writing " + keys.size() + " members");
+			for(String k : keys) {
+				if(k.equals("__proto__")) { //TODO hard coded for js
+					continue;
+				}
+				System.out.println("key: " + k + " value: " + v.getMember(k));
+				writeHelper(v.getMember(k));
+			}
+		}
+	}
+
+	public void close() throws Exception {
+		out.close();
+		out = null;
+	}
+}

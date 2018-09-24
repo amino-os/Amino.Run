0f7b3753489ecc071229df816827bc352fbbd4e9
Sungwook Moon <sungwook.moon@huawei.com>
2018-09-23 18:45:23 -0700

    2018-9-20  bug-fix: server policy should use group policy stub when replicated
    This is Hong's bug fix on groupPolicyStub applied to other multi_DM branch.

--- sapphire/sapphire-core/src/main/java/sapphire/runtime/Sapphire.java
+++ sapphire/sapphire-core/src/main/java/sapphire/runtime/Sapphire.java
@@ -119,7 +119,7 @@ public class Sapphire {
 
         if (policyChain == null || policyChain.size() == 0) return null;
         String policyName = policyChain.get(0).getPolicyName();
-        SapphireGroupPolicy existingGroupPolicy = policyChain.get(0).getGroupPolicy();
+        SapphireGroupPolicy existingGroupPolicy = policyChain.get(0).getGroupPolicyStub();
         AppObjectStub appStub = null;
 
         /* Get the annotations added for the Application class. */
@@ -228,7 +228,7 @@ public class Sapphire {
         serverPolicy.setNextDMs(nextPoliciesToCreate);
 
         SapphirePolicyContainer processedPolicy =
-                new SapphirePolicyContainerImpl(policyName, groupPolicy);
+                new SapphirePolicyContainerImpl(policyName, groupPolicyStub);
         processedPolicy.setServerPolicy(serverPolicy);
         processedPolicy.setServerPolicyStub((KernelObjectStub) serverPolicyStub);
         processedPolicy.setKernelOID(serverPolicy.$__getKernelOID());

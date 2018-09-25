6b37f18704c7aae7100909612e8b67b13c589364
Terry Zhuo <terryzhuo@Terrys-MacBook-Pro.local>
2018-09-24 23:31:19 -0700

    Remove useless files in DCAP_Sapphire directory

--- deployment/tests/local_int_cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-port_oms=22343
-port_client=22344
-
-echo "kill oms & kernel server..."
-netstat -tpln 2>>/dev/null | grep -E "${port_oms}|${port_kernel}" | awk '{print $(NF)}' | cut -d '/' -f 1 | xargs kill
-

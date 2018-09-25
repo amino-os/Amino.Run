76421706e2923fe33734ea105d32c95317a68361
a00903926 <amit.roushan@huawei.com>
2018-09-26 00:59:59 +0530

    Hanks TODO ruby app

---
+++ sapphire/examples/hanksTodoRuby/src/main/ruby/sapphire/appexamples/hankstodo/todo_list.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+
+class TodoList
+    def initialize(name)
+        @to_dos = Array.new
+        @name = name
+    end
+
+    def add_todo(todo)
+        @to_dos.push(todo)
+        puts "add #{todo} success!!!"
+        return "OK!"
+    end
+
+    def complete_todo(todo)
+        @to_dos.delete(todo)
+        puts "#{todo} task removed"
+        return "OK!"
+    end
+
+end

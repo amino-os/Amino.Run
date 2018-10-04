755825acd6de581fb18f71c8ad5a533a41702f2d
Hong Chen <hong.chen2@huawei.com>
2018-10-04 09:16:04 -0700

    pinkis: applied DHT policy to

--- sapphire/examples/pinkis/src/main/java/sapphire/demo/pinkisServer.java
+++ sapphire/examples/pinkis/src/main/java/sapphire/demo/pinkisServer.java
@@ -7,21 +7,24 @@ import java.io.Serializable;
 import java.util.Hashtable;
 import java.util.Map;
 
-@SapphireConfiguration(Policies = "sapphire.policy.DefaultSapphirePolicy")
+@SapphireConfiguration(Policies = "sapphire.policy.dht.DHTPolicy")
 public class pinkisServer implements SapphireObject {
     private Map<String, Serializable> kvStore = new Hashtable<>();
 
     public pinkisServer() {}
 
     public void set(String key, Serializable value) {
+        System.out.println("setting: " + key + " = " + value);
         this.kvStore.put(key, value);
     }
 
     public Serializable get(String key) {
+        System.out.println("getting: " + key);
         return this.kvStore.get(key);
     }
 
     public Boolean contains(String key) {
+        System.out.println("querying: " + key);
         return this.kvStore.containsKey(key);
     }
 }

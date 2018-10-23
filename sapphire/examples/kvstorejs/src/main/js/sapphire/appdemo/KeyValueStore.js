4d2b6a4e52541f4dc190927e972c51699fd05ec8
haibinxie <haibinxie@haibinxies-MacBook-Pro.local>
2018-10-23 15:39:50 -0700

    add key value store javascript demo app

---
+++ sapphire/examples/kvstorejs/src/main/js/sapphire/appdemo/KeyValueStore.js
@@ -0,0 +1,30 @@
+class KeyValueStore {
+    constructor() {
+        this.map = new Map();
+    }
+
+    set(key, value) {
+        this.map.set(key, value);
+        return true;
+    }
+
+    get(key) {
+        return this.map.get(key);
+    }
+
+    contains(key) {
+        return this.map.has(key);
+    }
+}
+
+function test() {
+    var store = new KeyValueStore();
+    store.set("key1", "value1");
+    console.log(store.get("key1"));
+    store.set("key1", 2);
+    console.log(store.get("key1"));
+    console.log(store.contains("key1"));
+}
+
+// For test purpose only
+//test();

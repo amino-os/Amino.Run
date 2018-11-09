07c51a854484e78b1427a14ee0b11594ce8bf58f
Sungwook Moon <sungwook.moon@huawei.com>
2018-11-08 16:02:37 -0800

    HanksToDo update to show multiDM scenario better.
    Previous HanksToDo shows results appropriate for DHTPolicy2. As DHTPolicy2 is removed, this commit updates HanksToDo to show the demo better.

--- sapphire/examples/hanksTodo/src/main/java/sapphire/appexamples/hankstodo/TodoList.java
+++ sapphire/examples/hanksTodo/src/main/java/sapphire/appexamples/hankstodo/TodoList.java
@@ -1,51 +1,49 @@
 package sapphire.appexamples.hankstodo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import sapphire.app.*;
 import sapphire.runtime.SapphireConfiguration;
 
 public class TodoList implements SapphireObject {
-	ArrayList<String> toDos = new ArrayList<String>();
-	String id = "0";
+    HashMap<String, String> toDos;
+    String id = "0";
 
-	public TodoList(String id) {
-		toDos = new ArrayList<String>();
-		this.id = id;
-	}
+    public TodoList(String id) {
+        toDos = new HashMap<>();
+        this.id = id;
+    }
 
-	/**
-	 * TODO: With DHTPolicy, TODO item should add with id so that it can be pulled by id.
-	 * @param id
-	 * @return
-	 */
-	public String addToDo(String id, String todo) {
-		System.out.println("TodoList>> id: " + id + " addToDo: " + todo);
-		toDos.add(todo);
-		return "OK!";
-	}
+    /**
+     * Add to do list content with subject and content.
+     *
+     * @param subject
+     * @param content
+     * @return
+     */
+    public String addToDo(String subject, String content) {
+        System.out.println("TodoList>> subject: " + subject + " addToDo: " + content);
+        String oldContent = toDos.get(subject);
+        oldContent = (oldContent == null) ? "" : oldContent + ", ";
+        String newContent = oldContent + content;
+        toDos.put(subject, newContent);
+        return "OK!";
+    }
 
-	/**
-	 * TODO: With DHTPolicy, TODOs should be picked up based on the input param - id.
-	 * @param id
-	 * @return
-	 */
-	public String getToDoString(String id) {
-		StringBuilder sb = new StringBuilder();
+    /**
+     * Get to do list content based on subject.
+     *
+     * @param subject
+     * @return
+     */
+    public String getToDo(String subject) {
+        return toDos.get(subject);
+    }
 
-		for (String toDo: toDos) {
-			sb.append(toDo);
-			sb.append(" : ");
-		}
+    public void completeToDo(String content) {}
 
-		sb.append("\n");
-		return sb.toString();
-	}
-
-	public void completeToDo(String todo) {
-	}
-
-	public ArrayList<Object> getHighPriority() {
-		return new ArrayList<Object>();
-	}
+    public ArrayList<Object> getHighPriority() {
+        return new ArrayList<Object>();
+    }
 }

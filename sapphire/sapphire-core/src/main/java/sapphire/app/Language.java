6edaf46d0bed1fd1b9a3ae55c0de7418397ac50d
Isaac Ackerman <h2ccoch3@gmail.com>
2018-12-21 12:41:13 -0800

    Interface upgrade for app/Language.java

--- sapphire/sapphire-core/src/main/java/sapphire/app/Language.java
+++ sapphire/sapphire-core/src/main/java/sapphire/app/Language.java
@@ -1,29 +1,51 @@
 package sapphire.app;
 
 /**
- * Programming languages supported by Sapphire
- *
- * <p>WARNING: String values of this enum will be used in {@code polyglot.eval} function and
- * therefore they should be consistent with the definitions in GraalVM.
+ * Programming languages supported by Sapphire.
  */
 public enum Language {
-    java("java"),
-    ruby("ruby"),
-    python("python"),
-    js("js"),
-    R("R");
+    java("java", true),
+    ruby("ruby", true),
+    python("python", false),
+    js("js", true),
+    R("R", false);
 
     private final String graalID;
+    private final boolean isSupported;
 
-    private Language(String id) {
+    private Language(String id, boolean support) {
         graalID = id;
+        isSupported = support;
     }
 
-    public String toString() {
+    /**
+     * Language supports JVM Reflection.
+     **/
+    public boolean supportJavaReflect() {
+        return this == Language.java;
+    }
+
+    /**
+     * Language supports GraalVM Reflection.
+     * Objects from this language can be cast to Value and
+     * fields and methods can be queried there.
+     **/
+    public boolean supportGraalVMReflect() {
+        //TODO this isn't futureproof
+        return this != Language.java;
+    }
+
+    /**
+     * Language ID for interop with GraalVM.
+     **/
+    public String graalID() {
         return graalID;
     }
 
-    public boolean isHostLanguage() {
-        return this == Language.java;
+    /**
+     * Language is supported by the Sapphire Runtime.
+     **/
+    public boolean isSupported() {
+        return isSupported;
     }
 }

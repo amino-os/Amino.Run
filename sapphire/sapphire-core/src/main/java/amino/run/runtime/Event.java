508c569eeb4d7194963811c49fd9a6673fe25336
AmitRoushan <amitroushan.cusat11@gmail.com>
2019-02-12 18:38:55 +0530

    eventhandler modifications

--- sapphire/sapphire-core/src/main/java/amino/run/runtime/Event.java
+++ sapphire/sapphire-core/src/main/java/amino/run/runtime/Event.java
@@ -2,12 +2,18 @@ package amino.run.runtime;
 
 public class Event {
     private String method;
+    private String name;
 
-    public Event(String method) {
+    public Event(String name, String method) {
         this.method = method;
+        this.name = name;
     }
 
     public String getMethod() {
         return method;
+    }
+
+    public String getName() {
+        return name;
     }
 }

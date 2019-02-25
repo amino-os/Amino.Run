f4fe76479f56e734e69393300bda2a777ad60da0
AmitRoushan <amitroushan.cusat11@gmail.com>
2019-02-25 12:37:52 +0530

    EventHandler modifications

--- sapphire/sapphire-core/src/main/java/amino/run/runtime/EventHandler.java
+++ sapphire/sapphire-core/src/main/java/amino/run/runtime/EventHandler.java
@@ -1,5 +1,6 @@
 package amino.run.runtime;
 
+import amino.run.runtime.exception.EventNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -7,14 +8,17 @@ import java.io.Serializable;
 import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.logging.Logger;
 
 /**
  * Event handler holds the group policy stub or server policy stub object of the sapphire object or
  * sapphire replica respectively created on the particular kernel server.
  */
 public class EventHandler implements Serializable {
+    static Logger logger = Logger.getLogger(EventHandler.class.getCanonicalName());
 
     private class PolicyHandler {
         private Object policyObject;
@@ -33,9 +37,11 @@ public class EventHandler implements Serializable {
     private InetSocketAddress host;
     private ArrayList<Object> objects;
     private Hashtable<String, PolicyHandler> handlers;
+    private HashMap<String, String> eventMapper;
 
     private void fillMethodTable() {
         handlers = new Hashtable<String, PolicyHandler>();
+        eventMapper = new HashMap<String, String>();
         Iterator<Object> it = objects.iterator();
         while (it.hasNext()) {
             Object obj = it.next();
@@ -43,10 +49,25 @@ public class EventHandler implements Serializable {
             // Grab the methods of the class
             Method[] methods = cl.getDeclaredMethods();
             for (int i = 0; i < methods.length; i++) {
-                PolicyHandler handler = new PolicyHandler(obj, methods[i]);
-                handlers.put(methods[i].toGenericString(), handler);
+                try {
+                    if (methods[i].isAnnotationPresent(AddEvent.class)) {
+                        PolicyHandler handler = new PolicyHandler(obj, methods[i]);
+                        String methodGenName = methods[i].toGenericString();
+                        handlers.put(methodGenName, handler);
+                        eventMapper.put(
+                                canonicalEventName(
+                                        cl, methods[i].getAnnotation(AddEvent.class).event()),
+                                methodGenName);
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.toString());
+                }
             }
         }
+    }
+
+    private String canonicalEventName(Class c, String event) {
+        return c.getName() + "_" + event;
     }
 
     public EventHandler(InetSocketAddress hostAddr, ArrayList<Object> policies) {
@@ -55,19 +76,24 @@ public class EventHandler implements Serializable {
         fillMethodTable();
     }
 
+    private Object invoke(String method, ArrayList<Object> params) throws Exception {
+        return handlers.get(method).invokeHandler(params);
+    }
+
     /**
      * Invoke method on the object using the params
      *
-     * @param method
+     * @param event
      * @param params
      * @return the return value from the method
      */
-    public Object invoke(String method, ArrayList<Object> params) throws Exception {
-        if (handlers.containsKey(method)) {
-            return handlers.get(method).invokeHandler(params);
-        } else {
-            return null;
+    public Object notifyEvent(Class c, String event, ArrayList<Object> params) throws Exception {
+        String canonicalEventName = canonicalEventName(c, event);
+        if (!eventMapper.containsKey(canonicalEventName)) {
+            throw new EventNotFoundException(
+                    String.format("Event %s not available in event handler", event));
         }
+        return invoke(eventMapper.get(canonicalEventName), params);
     }
 
     public InetSocketAddress getHost() {

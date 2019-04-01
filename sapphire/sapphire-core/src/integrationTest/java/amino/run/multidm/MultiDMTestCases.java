656516d904ab13479ec7bb84c75a76b16bc3ba95
Sungwook Moon <12723038+sungwook-moon@users.noreply.github.com>
2019-04-01 14:23:08 -0700

    Fixes #641: Integration tests for three/four DMs. (#645)

--- sapphire/sapphire-core/src/integrationTest/java/amino/run/multidm/MultiDMTestCases.java
+++ sapphire/sapphire-core/src/integrationTest/java/amino/run/multidm/MultiDMTestCases.java
@@ -10,6 +10,7 @@ import amino.run.kernel.server.KernelServerImpl;
 import java.io.File;
 import java.net.InetSocketAddress;
 import java.rmi.registry.LocateRegistry;
+import java.util.logging.Logger;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
@@ -21,10 +22,15 @@ import org.junit.Test;
  * Test <strong>multi-dm</strong> deployment managers, DHT & Consensus , DHT & MasterSlave,
  * AtLeastOnceRPC & DHT & Consensus , AtLeastOnceRPC & DHT & MasterSlave with multiple kernel
  * servers are covered here.
+ *
+ * <p>Every test in this class tests microservice specifications in <code>
+ * src/integrationTest/resources/specs/multi-dm</code> directory.
  */
 public class MultiDMTestCases {
+    final String MULTI_DM_PATH = "specs/multi-dm/";
     Registry registry;
     private static String regionName = "";
+    static Logger logger = java.util.logging.Logger.getLogger(MultiDMTestCases.class.getName());
 
     @BeforeClass
     public static void bootstrap() throws Exception {
@@ -40,14 +46,15 @@ public class MultiDMTestCases {
                 new InetSocketAddress(hostIp, hostPort), new InetSocketAddress(omsIp, omsPort));
     }
 
-    private void runTest(MicroServiceSpec spec, boolean consensus) throws Exception {
+    private void runTest(String testName) throws Exception {
+        File file = getResourceFile(MULTI_DM_PATH + testName + ".yaml");
+        MicroServiceSpec spec = readMicroServiceSpec(file);
         MicroServiceID microServiceId = registry.create(spec.toString());
         KVStore store = (KVStore) registry.acquireStub(microServiceId);
         // consensus DM needs some time to elect the leader other wise function call will fail
-        if (consensus) {
+        if (testName.contains("Consensus")) {
             Thread.sleep(5000);
         }
-
         for (int i = 0; i < 10; i++) {
             String key = "k1_" + i;
             String value = "v1_" + i;
@@ -57,58 +64,106 @@ public class MultiDMTestCases {
         }
     }
 
-    /**
-     * Test microservice specifications in <code>src/integrationTest/resources/specs/multi-dm
-     * </code> directory.
-     *
-     * @throws Exception
-     */
     @Test
-    public void testDHTNConsensusMultiDM() throws Exception {
-        File file = getResourceFile("specs/multi-dm/DHTNConsensus.yaml");
-        MicroServiceSpec spec = readMicroServiceSpec(file);
-        runTest(spec, true);
+    public void testDHTConsensus() throws Exception {
+        runTest("DHTConsensus");
     }
 
-    /**
-     * Test microservice specifications in <code>src/integrationTest/resources/specs/multi-dm
-     * </code> directory.
-     *
-     * @throws Exception
-     */
     @Test
-    public void testDHTNMasterSlaveMultiDM() throws Exception {
-        File file = getResourceFile("specs/multi-dm/DHTNMasterSlave.yaml");
-        MicroServiceSpec spec = readMicroServiceSpec(file);
-        runTest(spec, false);
+    public void testDHTConsensusAtleastRPC() throws Exception {
+        runTest("DHTConsensusAtleastRPC");
     }
 
-    /**
-     * Test microservice specifications in <code>src/integrationTest/resources/specs/multi-dm
-     * </code> directory.
-     *
-     * @throws Exception
-     */
+    @Test
+    public void testDHTConsensusAtleastRPCCacheLease() throws Exception {
+        runTest("DHTConsensusAtleastRPCCacheLease");
+    }
+
+    @Test
+    public void testDHTConsensusCacheLease() throws Exception {
+        runTest("DHTConsensusCacheLease");
+    }
+
+    @Test
+    public void testDHTConsensusCacheLeaseAtleastRPC() throws Exception {
+        runTest("DHTConsensusCacheLeaseAtleastRPC");
+    }
+
+    @Test
+    public void testDHTMasterSlave() throws Exception {
+        runTest("DHTMasterSlave");
+    }
+
+    @Test
+    public void testDHTMasterSlaveAtleastRPC() throws Exception {
+        runTest("DHTMasterSlaveAtleastRPC");
+    }
+
+    @Test
+    public void testDHTMasterSlaveAtleastRPCCacheLease() throws Exception {
+        runTest("DHTMasterSlaveAtleastRPCCacheLease");
+    }
+
+    @Test
+    public void testDHTMasterSlaveCacheLease() throws Exception {
+        runTest("DHTMasterSlaveCacheLease");
+    }
+
     @Test
     @Ignore("Test is ignored will be removed once the multi DM issues are resolved")
-    public void testAtleastOnceRPCDHTNMasterSlaveMultiDM() throws Exception {
-        File file = getResourceFile("specs/multi-dm/AtleastRPCDHTNMasterSlave.yaml");
-        MicroServiceSpec spec = readMicroServiceSpec(file);
-        runTest(spec, false);
+    public void testAtleastRPCDHTMasterSlave() throws Exception {
+        runTest("AtleastRPCDHTMasterSlave");
     }
 
-    /**
-     * Test microservice specifications in <code>src/integrationTest/resources/specs/multi-dm
-     * </code> directory.
-     *
-     * @throws Exception
-     */
     @Test
     @Ignore("Test is ignored will be removed once the multi DM issues are resolved")
-    public void testAtleastOnceRPCDHTNConsensusMultiDM() throws Exception {
-        File file = getResourceFile("specs/multi-dm/AtleastRPCDHTNConsensus.yaml");
-        MicroServiceSpec spec = readMicroServiceSpec(file);
-        runTest(spec, true);
+    public void testAtleastRPCDHTConsensus() throws Exception {
+        runTest("AtleastRPCDHTConsensus");
+    }
+
+    @Test
+    public void testAtleastRPCCacheLease() throws Exception {
+        runTest("AtleastRPCCacheLease");
+    }
+
+    @Test
+    public void testAtleastRPCCacheLeaseConsensus() throws Exception {
+        runTest("AtleastRPCCacheLeaseConsensus");
+    }
+
+    @Test
+    public void testAtleastRPCCacheLeaseDHTConsensus() throws Exception {
+        runTest("AtleastRPCCacheLeaseDHTConsensus");
+    }
+
+    @Test
+    public void testAtleastRPCCacheLeaseMasterSlave() throws Exception {
+        runTest("AtleastRPCCacheLeaseMasterSlave");
+    }
+
+    @Test
+    public void testCacheLeaseAtleastRPC() throws Exception {
+        runTest("CacheLeaseAtleastRPC");
+    }
+
+    @Test
+    public void testCacheLeaseAtleastRPCDHTConsensus() throws Exception {
+        runTest("CacheLeaseAtleastRPCDHTConsensus");
+    }
+
+    @Test
+    public void testCacheLeaseDHT() throws Exception {
+        runTest("CacheLeaseDHT");
+    }
+
+    @Test
+    public void testCacheLeaseDHTMasterSlave() throws Exception {
+        runTest("CacheLeaseDHTMasterSlave");
+    }
+
+    @Test
+    public void testCacheLeaseDHTConsensus() throws Exception {
+        runTest("CacheLeaseDHTConsensus");
     }
 
     @AfterClass
